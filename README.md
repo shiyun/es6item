24fd6be1b129974d6b97988a90a4438b5352a9ef
shiyun <shiyun1013@163.com>
2017-03-01 16:28:26 +0800

    item add

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
-# usees6
 =======
 # es6item
->>>>>>> a7f8456130eade054eea506f3f9abdd20344c691
--- src/js/index.js
+++ src/js/index.js
@@ -1,34 +1,55 @@
 'use strict';
-import logout from './util/logout'
-import setBg from './util/setBg'
 
-if($('.productWrap').length){
-	setBg();
+let counter = 0;
+const testData = [
+	{cont: 11},
+	{cont: 12},
+	{cont: 13},
+]
+
+const getData = data => {
+	let _html = `<li class="fadeIn animated"><ul class="list-photo">`;
+	if(Array.isArray(data)){
+		data.map(v => {
+			_html += `
+				<li>
+					<img src="../images/loading.gif" class="leftImg">
+					<div class="cont">
+						<p class="contInfo">${v.cont}</p>
+					</div>
+				</li>
+			`;
+		});
+		_html += `</ul></li>`;
+	}else{
+		alert('数据错误')
+	}
+	return _html;
 }
-logout();
 
-/*
-ajax.post(ajax.api.LOGIN, {"organizeId":"1004","secret":"C6F215DAB70B852CED70853BE873DE3B"})
-	.then(res=>{
-		const data = res.content;
-	 	const token = data.token;
-	 	console.log(token);
-	 	ajax.post(ajax.api.VERIFYUNIQUECODE,{"token":token,"organizeId":"1004","uniqueCode":"1234543"})
-	 		.then(res=>{
-	 			console.log('VERIFYUNIQUECODE:',res);
-	 		})
-	 		.catch(err=>console.log(err));
+setInterval(()=>{
+	const $listItem = $('.list-item');
+	if($listItem.find('>li').length >= 3){
+		$listItem.animate({'margin-left': '-33.3%'}, 1000, ()=>{
+			$listItem.css({'margin-left': 0}).find('>li:eq(0)').remove();
+			$listItem.append(getData(testData));
+		});
+	}else{
+		$listItem.append(getData(testData));
+	}
+}, 2000)
 
-	 	ajax.post(ajax.api.INFOADD,{"token":token,"organizeId":"1004","name":"test","organType":"0","secret":"C6F215DAB70B852CED70853BE873DE3B","organCode":"3432","legalName":"test","legalIdNo":"3432","email":"test@fabao.cn","mobile":"13524898984"})
-	 		.then(res=>{
-	 			console.log('INFOADD:',res);
-	 		})
-	 		.catch(err=>console.log(err));
-
-	 	ajax.post(ajax.api.TESTIFYLIST,{"token":token,"organizeId":"1004","uniqueCode":"1234543"})
-	 		.then(res=>{
-	 			console.log('TESTIFYLIST:',res);
-	 		})
-	 		.catch(err=>console.log(err));
-	})
-	.catch(err=>console.log(err));*/
+$.ajax({
+    type: 'POST',
+    url: 'http://www.bao.com',
+    data: {id: 1},            
+    dataType: "json",
+    success: res => {
+    	consoe.log(1);
+    	let data = res || testData;
+    	getData(data);
+    },
+    error: err => {
+    	console.log(2)
+    }
+});
